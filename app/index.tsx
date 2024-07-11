2274e3d6c721158baa13d1c9dcf9d13d9e05dc69
Remberto Villar <84630788+R-Villar@users.noreply.github.com>
2024-07-11 12:25:11 -0400

    homescreen setup.

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,27 @@
-import { Image, StyleSheet, Platform, View, Text } from "react-native"
+import { View, Text, ImageBackground, SafeAreaView } from "react-native"
+import { LinearGradient } from "expo-linear-gradient"
+import { StatusBar } from "expo-status-bar"
 
-export default function HomeScreen() {
+import beachImage from "@/assets/meditation-images/beach.webp"
+const App = () => {
 	return (
-		<View style={styles.container}>
-			<Text>Hello world!</Text>
+		<View className='flex-1'>
+			<ImageBackground source={beachImage} resizeMode='cover' className='flex-1'>
+				<LinearGradient className='flex-1' colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}>
+					<SafeAreaView className='flex-1 px-1 justify-between'>
+						<View>
+							<Text className='text-center text-white font-bold text-4xl'>
+								Simple Meditation
+							</Text>
+							<Text className='text-center text-white text-regular text-2xl mt-3'>
+								Simplifying Meditation for Everyone
+							</Text>
+						</View>
+						<StatusBar style='light' />
+					</SafeAreaView>
+				</LinearGradient>
+			</ImageBackground>
 		</View>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-	},
-})
+export default App
